22741de482498169dbe4d1fe1f040c8f14941936
Majd <majd.lefi@esprit.tn>
2021-01-11 11:05:09 +0100

    update views

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const path = require('path')
 const app = express()
 const port = 3000;
 
+const currentDate = new Date();
+const day = currentDate.getDay();
+const hours = currentDate.getHours();
+
+app.use((req, res, next) => {
+  if (day >= 1 && day <= 5 && hours >= 10 && hours < 17) {
+    next();
+  } else {
+    res.send("Out of service !");
+  }
+});
+
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+app.get('/style.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/style.css'));
 });
 
 app.get('/about',function(req,res){
@@ -15,6 +31,10 @@ app.get('/about',function(req,res){
 
 app.get('/services',function(req,res){
   res.sendFile(path.join(__dirname+'/public/services.html'));
+  
+app.get('/contactus',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/contactus.html'));
+});
 });
 
 app.listen (port , () => {
